a339763189f6fbd22bdc56f606db22e4aaaa47de
AbdelrahmanElmeniawy <52868473+AbdelrahmanElmeniawy@users.noreply.github.com>
2021-12-07 04:22:58 +0200

    Add files via upload

---
+++ first part/Activity.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Activity {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+	    System.out.print("Enter file absolute path: ");
+	    String filepath = scanner.nextLine();
+	    File inputfile = new File(filepath);
+	    Scanner reader = new Scanner(inputfile);
+	    int size = reader.nextInt();
+	    Act[] list = new Act[size];
+	    for(int i = 0; i < size; i++) {
+	    	list[i] = new Act(reader.nextInt(), reader.nextInt(), reader.nextInt());
+	    }
+	    int ans = select(list);
+	    String outputfile = filepath.substring(0, filepath.length() - 3);
+	    FileWriter myWriter = new FileWriter(outputfile + "_18012538.out");
+	    myWriter.write(Integer.toString(ans));
+	    myWriter.close();
+	    System.out.println("Finished");
+	}
+	private static class Act{
+		private int start, end, weight;
+		private Act(int s, int e, int w){
+			start = s; end = e; weight = w;
+		}
+	}
+	private static int select(Act[] list) {
+        Arrays.sort(list, (a, b)->a.end - b.end);
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0, 0);
+        for (Act activity : list) {
+            int cur = dp.floorEntry(activity.start).getValue() + activity.weight;
+            if (cur > dp.lastEntry().getValue())
+                dp.put(activity.end, cur);
+        }
+        return dp.lastEntry().getValue();
+    }
+
+}
